e1f725e14d79ff8a15c588b9b989d0486666575b
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-08-26 18:35:19 +0530

    bubble tea

--- bubbletea/sample.go
+++ bubbletea/sample.go
@@ -5,3 +5,17 @@ import (
     "os"
     tea "github.com/charmbracelet/bubbletea"
 )
+
+type model struct {
+    choices []string
+    cursor int
+    selected map[int]struct{}
+}
+
+func initialModel() model {
+    return model{
+         choices: []string{"Buy Carrots", "Buy Apples", "Buy Tomatos"},
+         selected: make(map[int]struct{}),
+    }
+
+}
