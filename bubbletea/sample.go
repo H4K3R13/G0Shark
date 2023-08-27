32a5eac361678e888030996dd869d73ccd5d5b26
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-08-27 18:51:35 +0530

    Update method

--- bubbletea/sample.go
+++ bubbletea/sample.go
@@ -17,5 +17,36 @@ func initialModel() model {
          choices: []string{"Buy Carrots", "Buy Apples", "Buy Tomatos"},
          selected: make(map[int]struct{}),
     }
-
 }
+
+func (m model) Init() tea.Cmd{
+    return nil
+}
+
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+    case tea.KeyMsg:
+        switch msg.String() {
+        case "ctrl+c", "q":
+            return m, tea.Quit
+        case "up", "k":
+            if m.cursor > 0 {
+                m.cursor--
+            }
+        case "down", "j":
+            if m.cursor < len(m.choices)-1 {
+                m.cursor++
+            }
+        case "enter", " ":
+            _, ok := m.selected[m.cursor]
+            if ok {
+                delete(m.selected, m.cursor)
+            } else {
+                m.selected[m.cursor] = struct{}{}
+            }
+        }
+    }
+    return m, nil
+}
+
