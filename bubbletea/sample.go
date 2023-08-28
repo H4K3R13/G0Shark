959ce927cef3ef7b639b4c484c5c69a6360b4495
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-08-28 18:33:53 +0530

    bubble tea complete

--- bubbletea/sample.go
+++ bubbletea/sample.go
@@ -23,7 +23,6 @@ func (m model) Init() tea.Cmd{
     return nil
 }
 
-
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg := msg.(type) {
     case tea.KeyMsg:
@@ -50,3 +49,29 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     return m, nil
 }
 
+func (m model) View() string {
+    s := "What should we buy at the market?\n\n"
+    for i, choice := range m.choices {
+        cursor := " " 
+        if m.cursor == i {
+            cursor = ">" // cursor!
+        }
+        checked := " " 
+        if _, ok := m.selected[i]; ok {
+            checked = "x" // selected!
+        }
+
+        // Render the row
+        s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+    }
+    s += "\nPress q to quit.\n"
+    return s
+}
+
+func main() {
+    p := tea.NewProgram(initialModel())
+    if _, err := p.Run(); err != nil {
+        fmt.Printf("Alas, there's been an error: %v", err)
+        os.Exit(1)
+    }
+}
