a4faaa5534edde7022f6e57e70eba55376983849
ARJUN C <50370450+H4K3R13@users.noreply.github.com>
2023-09-11 19:38:55 +0530

    enchm. of help menu

--- pkg/mypackage/help.go
+++ pkg/mypackage/help.go
@@ -1,15 +1,32 @@
 package mypackage
-import(
-	"github.com/pterm/pterm"
+
+import (
 	"fmt"
+	"github.com/pterm/pterm"
 )
-func Help(){
-	pterm.DefaultTable.WithHasHeader().WithBoxed().WithData(pterm.TableData{
-		{"Option", "Function", "Example"},
-		{"-h", "help", ""},
+
+// Option represents a command-line option with its function and example.
+type Option struct {
+	Command   string
+	Function  string
+	Example   string
+}
+
+// Help displays the available command-line options.
+func Help() {
+	options := []Option{
+		{"-h", "Help", ""},
 		{"-r", "Read .pcap files", "-r filename/filepath"},
-		{"-s", "Scan an IP", "-s en0 $IP [port,port,port]or[port-port] "},	
-		
-	}).Render()
+		{"-s", "Scan an IP", "-s en0 $IP [port,port,port]or[port-port]"},
+	}
+
+	table := pterm.TableData{}
+	table = append(table, []string{"Option", "Function", "Example"})
+
+	for _, opt := range options {
+		table = append(table, []string{opt.Command, opt.Function, opt.Example})
+	}
+
+	pterm.DefaultTable.WithHasHeader().WithBoxed().WithData(table).Render()
 	fmt.Println("Select the packets from the option selector")
 }
